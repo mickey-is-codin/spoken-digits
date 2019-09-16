8b9e306def77b715fffa1408bc868d20b6c15d14
Mickey Smith <smithmick14@gmail.com>
2019-09-16 13:11:56 -0500

    Removed the test directory for now. Set up makefile to work with data_intro.py. Going to look into the wave library now

---
+++ Makefile
@@ -0,0 +1,7 @@
+PY=python3
+CACHE=__pycache__/
+
+SRC_DATA=src/data_intro.py
+
+eda:
+	$(PY) $(SRC_DATA)
--- src/data_intro.py
+++ src/data_intro.py
@@ -9,9 +9,15 @@ import matplotlib.pyplot as plt
 
 def main():
 
+
     print("Beginning program execution...")
 
+    # Dummy variables (likely to be cmdline args later)
+    data_root = 'data/'
 
+    data_path = data_root + 'recordings/'
+
+    print("Dataset contains {} overall files".format(len(os.listdir(data_path))))
 
 
 if __name__ == '__main__':
